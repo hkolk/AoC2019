666495a617930187acd3245e4cb556f62a0cea81
Harm Kolk <hkolk@ebay.com>
2019-12-08 23:59:35 +0100

    Some cleanup, bringing it back to a single implementation

--- day07/src/main.rs
+++ day07/src/main.rs
@@ -14,9 +14,16 @@ fn main() {
     part2(&memory);
 }
 
+fn part1(memory: &Vec<isize>) {
+    run_permutations(memory, vec![0, 1, 2, 3, 4])
+}
+
 fn part2(memory: &Vec<isize>) {
-    //let data = [3, 1, 4, 2, 0];
-    let mut data = [5, 6, 7, 8, 9];
+    run_permutations(memory, vec![5, 6, 7, 8, 9])
+}
+
+fn run_permutations(memory: &Vec<isize>, phase_options: Vec<isize>) {
+    let mut data = phase_options.clone();
     let mut permutations = Vec::new();
     heap_recursive(&mut data, |permutation| {
         permutations.push(permutation.to_vec())
@@ -32,16 +39,14 @@ fn part2(memory: &Vec<isize>) {
         while computers.len() > 0 {
             let mut computer = computers.pop_front().unwrap();
             computer.add_input(prev_value);
-            //println!("beginning next one at {:?}", computer);
+
             let mut new_computer = computer.run();
             match new_computer.state {
                 State::Halted => {
-                    //println!("Halted");
                     computers.push_back(computer);
                     prev_value = new_computer.output.pop().unwrap();
                 }
                 State::Terminated => {
-                    //println!("Terminated");
                     prev_value = computer.output.pop().unwrap();
                     running -= 1;
                     if running == 0 {
@@ -53,55 +58,14 @@ fn part2(memory: &Vec<isize>) {
                 }
             }
         }
-        //println!("Final value: {:?}", prev_value);
         if prev_value > max_output {
             max_output = prev_value;
             max_phases = phases.clone();
         }
     }
-    println!("Part 2: Max Phases {:?} generated {:?}", max_phases, max_output);
-
-
-    /*
-    let (final_state, output) = run(memory,  &vec![5]);
-    println!("memory: {:?}", final_state);
-    println!("output: {:?}", output);
-    println!("Part 1 answer: {:?}", output.last().unwrap())
-    */
+    println!("Answer for input: {:?}: Max Phases {:?} generated {:?}", phase_options, max_phases, max_output);
 }
 
-fn part1(memory: &Vec<isize>) {
-    let mut data = [0, 1, 2, 3, 4];
-    let mut permutations = Vec::new();
-    heap_recursive(&mut data, |permutation| {
-        permutations.push(permutation.to_vec())
-    });
-
-    let mut max_output = 0;
-    let mut max_phases = Vec::new();
-
-    for phases in permutations {
-        let mut prev_output = 0;
-        for i in 0..5 {
-            let input = VecDeque::from(vec![phases[i], prev_output]);
-            let (_final_state, output) = run(memory,  &input);
-            prev_output = *output.last().unwrap();
-        }
-        if prev_output > max_output {
-            max_output = prev_output;
-            max_phases = phases.clone();
-        }
-        //println!("Phases {:?} generated {:?}", phases, prev_output);
-    }
-    println!("Part 1: Max Phases {:?} generated {:?}", max_phases, max_output);
-
-    /*
-    let (final_state, output) = run(memory,  &vec![5]);
-    println!("memory: {:?}", final_state);
-    println!("output: {:?}", output);
-    println!("Part 1 answer: {:?}", output.last().unwrap())
-    */
-}
 
 #[derive(Copy, Clone, Debug, PartialEq)]
 enum State {
@@ -169,8 +133,6 @@ impl IntComputer {
                     self.output.push(get_val1(&self.memory, self.iptr));
                     self.iptr += 2;
                     // halting state
-                    //println!("[{:?}] Halting because I just pushed data, which was {:?}", self.phase, self.output);
-                    //println!("{:?}", self);
                     self.state = State::Halted;
                     break;
                 }
@@ -200,63 +162,6 @@ impl IntComputer {
         }
         return self.clone();
     }
-}
-
-
-fn run(original_memory: &Vec<isize>, original_input: &VecDeque<isize>) -> (Vec<isize>, Vec<isize>) {
-    let mut memory = original_memory.clone();
-    let mut output :Vec<isize> = Vec::new();
-    let mut input = original_input.clone();
-
-    let mut iptr: usize = 0;
-    loop {
-        match memory[iptr] % 100 {
-            1 => {
-                let dest = memory[iptr + 3] as usize;
-                memory[dest] = get_val1(&memory, iptr) + get_val2(&memory, iptr);
-                iptr += 4;
-            }
-            2 => {
-                let dest = memory[iptr + 3] as usize;
-                memory[dest] = get_val1(&memory, iptr) * get_val2(&memory, iptr);
-                iptr += 4;
-            }
-            3 => {
-                if input.len() == 0 {
-                    // halting state
-                }
-                let dest = memory[iptr + 1] as usize;
-                memory[dest] = input.pop_front().unwrap();
-                iptr += 2;
-            }
-            4 => {
-                output.push(get_val1(&memory, iptr));
-                iptr += 2;
-            }
-            5 => {
-                iptr = if get_val1(&memory, iptr) > 0 { get_val2(&memory, iptr) as usize } else { iptr + 3 };
-
-            }
-            6 => {
-                iptr = if get_val1(&memory, iptr) == 0 { get_val2(&memory, iptr) as usize } else { iptr + 3 };
-            }
-            7 => {
-                let dest = memory[iptr + 3] as usize;
-                memory[dest] = if get_val1(&memory, iptr) < get_val2(&memory, iptr) { 1 } else { 0 };
-                iptr += 4;
-            }
-            8 => {
-                let dest = memory[iptr + 3] as usize;
-                memory[dest] = if get_val1(&memory, iptr) == get_val2(&memory, iptr) { 1 } else { 0 };
-                iptr += 4;
-            }
-            99 => {
-                break;
-            }
-            _ => panic!()
-        }
-    }
-    return (memory, output);
 }
 
 fn get_val1(memory: &Vec<isize>, iptr: usize) -> isize {
