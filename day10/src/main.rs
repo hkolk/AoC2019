9aa9e297fbaf47dade587a37c85e93ee4228aecb
Harm Kolk <hkolk@ebay.com>
2019-12-10 20:12:14 +0100

    Day10 part 1: eeeeuuuuwwww cleanup afterwards :)

---
+++ day10/src/main.rs
@@ -0,0 +1,106 @@
+use std::fs;
+use ndarray::Array;
+
+fn main() {
+    let input = "input.txt";
+    let contents = fs::read_to_string(input).expect("Error reading input");
+    let lines = contents.lines().map(|line| {
+        line.chars().map(|loc| loc == '#').collect()
+    }).collect::<Vec<Vec<_>>>();
+
+    let mut asteroids = Vec::new();
+    for (y, line) in lines.iter().enumerate() {
+        for (x, position) in line.iter().enumerate() {
+            if *position {
+                asteroids.push(Coord{ y: y as isize, x: x as isize });
+            }
+        }
+    }
+
+    let mut best_asteroid_count = 0;
+    let mut best_asteroid = &Coord {y: -1, x: -1};
+
+    for asteroid in &asteroids {
+        let mut visible_asteroids = -1; // I will find myself as wel...
+        for other_asteroid in &asteroids {
+            let path = asteroid.path(&other_asteroid);
+            let mut colission = false;
+            for path_coord in path {
+                if lines[path_coord.y as usize][path_coord.x as usize] {
+                    // colission!
+                    colission = true;
+                }
+            }
+            if !colission {
+                visible_asteroids += 1;
+            }
+        }
+        println!("Asterroid at {:?} can see {:?} other asteroids!", asteroid, visible_asteroids);
+        if visible_asteroids > best_asteroid_count {
+            best_asteroid = asteroid;
+            best_asteroid_count = visible_asteroids;
+        }
+    }
+    println!("!! Best asterroid at {:?} can see {:?} other asteroids!", best_asteroid, best_asteroid_count);
+
+
+
+
+    //let path = Coord{y:0, x:0}.path(&Coord{y:0, x:0});
+    //println!("{:#?}", path);
+
+
+    /*
+    let map = Array::from_shape_vec(
+        (lines.len(), lines.first().unwrap().len()),
+        lines.iter().flatten().collect::<Vec<_>>()
+    ).unwrap();
+    println!("{:#?}", map);
+
+    for ((y, x), item) in map.indexed_iter() {
+        println!("[{},{}] {:?}", y, x, item);
+    }
+    map
+    */
+}
+
+#[derive(Copy, Clone, Debug, PartialEq)]
+struct Coord {
+    x: isize,
+    y: isize
+}
+
+impl Coord {
+    fn path(&self, other: &Coord) -> Vec<Coord> {
+        let mut path = Vec::new();
+        let dist_x = other.x - self.x;
+        let dist_y = other.y - self.y;
+        let gcd = gcd(dist_x, dist_y) as isize;
+        if gcd == 0 {
+            return path;
+        }
+        let reduced_x = dist_x / gcd;
+        let reduced_y = dist_y / gcd;
+
+        //println!("{:?} {:?} {:?} {:?} {:?}", dist_x, dist_y, gcd, reduced_x, reduced_y);
+        for step in 1..gcd {
+            //println!("{:?}", step);
+            path.push(Coord{
+                x:self.x + (reduced_x * step),
+                y:self.y + (reduced_y * step),
+            })
+        }
+        return path;
+    }
+}
+
+fn gcd(x: isize, y: isize) -> usize {
+    let mut x = x.abs() as usize;
+    let mut y = y.abs() as usize;
+    while y != 0 {
+        let t = y;
+        y = x % y;
+        x = t;
+    }
+    x
+}
